4badb7dc870b0f8ccfde545be9f25d5a9d444964
Jeremiah Campbell <jeremiah.campbell@meantheory.com>
2016-06-23 16:36:27 -0400

    Jeremiah Campbell Gilded Rose Work

---
+++ python/rose.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+'''
+This entire problem is a horrible mess of confusion and bad ideas.
+'''
+
+backstage = "Backstage passes to a TAFKAL80ETC concert"
+brie = "Aged Brie"
+sulfuras = "Sulfuras, Hand of Ragnaros"
+cake = "Conjured Mana Cake"
+quality_max = 50
+quality_min = 0
+sell_in_threshold = 0
+
+class GildedRose(object):
+
+    def __init__(self, items):
+        self.items = items
+
+    def rules(self, item):
+        '''
+        This entire problem is a horrible mess of confusion and bad ideas.
+        '''
+
+        # rule 1 - increment quality
+        if item.name in [backstage, brie] \
+        and item.quality < quality_max:
+            item.inc('quality')
+
+        # rule 2 - increment quality
+        if item.name == backstage \
+        and item.quality < quality_max:
+            if item.sell_in < 11:
+                item.inc('quality')
+
+            if item.sell_in < 6:
+                item.inc('quality')
+
+        # rule 3 - decrease sell_in
+        if item.name != sulfuras:
+            item.dec('sell_in')
+
+        # rule 4 - decrease quality
+        if item.name not in [backstage, brie, sulfuras] \
+        and item.quality > quality_min:
+            item.dec('quality')
+
+        # rule 5 - decrease quality
+        if item.sell_in < sell_in_threshold \
+        and item.name not in [backstage, brie, sulfuras] \
+        and item.quality > quality_min:
+            item.dec('quality')
+
+        # rule 6 - decrease quality
+        if item.sell_in < sell_in_threshold \
+        and item.name == backstage:
+            item.dec('quality', item.quality)
+
+        # rule 7 - increase quality
+        if item.sell_in < sell_in_threshold \
+        and item.name == brie \
+        and item.quality < quality_max:
+            item.inc('quality')
+
+    def update_quality(self):
+        for item in self.items:
+            self.rules(item)
+
+class Item(object):
+
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def change(self, p, value):
+
+        if p == 'sell_in':
+            self.sell_in += value
+
+        if p == 'quality':
+            self.quality += value
+
+    def inc(self, p, value=1):
+        self.change(p, value)
+
+    def dec(self, p, value=-1):
+        if value > 0:
+            value = -value
+        self.change(p, value)
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,14 +1,85 @@
 # -*- coding: utf-8 -*-
-import unittest
+# import unittest
+import pytest
+from rose import Item
+from rose import GildedRose
 
-from gilded_rose import Item, GildedRose
+tstdata = [
+    (Item(name="+5 Dexterity Vest", sell_in=10, quality=20), [
+        [9, 19],
+        [8, 18],
+        [7, 17],
+        [6, 16],
+        [5, 15]
+    ]),
+    (Item(name="Aged Brie", sell_in=2, quality=0), [
+        [1, 1],
+        [0, 2],
+        [-1, 4],
+        [-2, 6],
+        [-3, 8],
+    ]),
+    (Item(name="Elixir of the Mongoose", sell_in=5, quality=7), [
+        [4, 6],
+        [3, 5],
+        [2, 4],
+        [1, 3],
+        [0, 2],
+    ]),
+    (Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80), [
+        [0, 80],
+        [0, 80],
+        [0, 80],
+        [0, 80],
+        [0, 80],
+    ]),
+    (Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80), [
+        [-1, 80],
+        [-1, 80],
+        [-1, 80],
+        [-1, 80],
+        [-1, 80],
+    ]),
+    (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20), [
+        [14, 21],
+        [13, 22],
+        [12, 23],
+        [11, 24],
+        [10, 25],
+    ]),
+    (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49), [
+        [9, 50],
+        [8, 50],
+        [7, 50],
+        [6, 50],
+        [5, 50],
+    ]),
+    (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49), [
+        [4, 50],
+        [3, 50],
+        [2, 50],
+        [1, 50],
+        [0, 50],
+    ]),
+    (Item(name="Conjured Mana Cake", sell_in=3, quality=6), [
+        [2, 5],
+        [1, 4],
+        [0, 3],
+        [-1, 1],
+        [-2, 0],
+    ]),
+]
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+@pytest.mark.parametrize("item,tstexpect", tstdata, scope="function")
+def test_modified(item, tstexpect):
+    gr = GildedRose(list([item]))
+    print(gr.items)
 
-if __name__ == '__main__':
-    unittest.main()
+    for expected in tstexpect:
+        gr.update_quality()
+        ref = gr.items[0]
+        print(expected, ref.sell_in, ref.quality)
+        assert expected == [ref.sell_in, ref.quality]
+
+# if __name__ == '__main__':
+#     unittest.main()
